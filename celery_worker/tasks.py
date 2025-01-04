4ef93681bf91d472911b69d86eadccd1833da2e8
Anastasios Anastasiadis <anastasiosanastasiadis@192.168.1.4>
2025-01-04 09:52:47 +0200

    change task to write and then update processing table

--- celery_worker/tasks.py
+++ celery_worker/tasks.py
@@ -8,32 +8,40 @@ import json
 @celery_app.task(bind=True)
 def process_url_task(self,url:str,user_id:int):
     db = SessionLocal()
+    # Enter an entry to the database
+    processing_result = ProcessingResult(
+        user_id=user_id,
+        url=url,
+        status="pending",
+        task_id=self.request.id,
+        created_at_utc=datetime.now(timezone.utc)
+    )
+    db.add(processing_result)
+    db.commit()
+
+
     try:
         try:
-            user = db.query(User).get(user_id)
-        except Exception as e:
-            print(f"Error getting user {user_id}: {str(e)}")
-            raise
-        try:
-            processor = SyncAsyncContentProcessor(user)
-            result = processor.process_url(url)
-        except Exception as e:
-            print(f"Error processing URL {url}: {str(e)}")
-            raise
+            try:
+                user = db.query(User).get(user_id)
+                processor = SyncAsyncContentProcessor(user)
+                result = processor.process_url(url)
+            except Exception as e:
+                print(f"Error processing URL {url}: {str(e)}")
+                raise
 
-        # Store result in database
-        processing_result = ProcessingResult(
-            user_id=user_id,
-            url=url,
-            status=result["status"],
-            tweets=json.dumps(result.get("tweets", [])),
-            tweet_count=result.get("tweet_count", 0),
-            error_message=result.get("message", None),
-            task_id=self.request.id,
-            created_at_utc=datetime.now(timezone.utc)
-        )
-        db.add(processing_result)
-        db.commit()
+            processing_result.status = result["status"]
+            processing_result.tweets = json.dumps(result.get("tweets", []))
+            processing_result.tweet_count = result.get("tweet_count", 0)
+            processing_result.error_message = result.get("message", None)
+            db.add(processing_result)
+            db.commit()
+        except Exception as e:
+            print(f"Error in either processing the url or inserting new result in the database {str(e)}")
+            processing_result.status = "failed"
+            processing_result.error_message = str(e)
+            db.commit()
+            raise
         
         return {
             "status": "success",
